57ec4c6a206cf22eb7860044631761e33d993e14
lukas <lukasalazarek@gmail.com>
2022-05-12 05:34:47 -0500

    Finish tests, making the interfaces uniform

--- either.rkt
+++ either.rkt
@@ -1,7 +1,5 @@
 #lang at-exp racket/base
 
-;; todo: finish the rest of the pkg necessities: info.rkt, tests, so on.
-
 ;; This design does not explicitly wrap successes.
 ;; I find that this is often a convenient way to work with these things,
 ;; but it does have one issue to be aware of when using the helpers
@@ -20,17 +18,21 @@
 (provide (struct-out failure)
          either/c
          either-let*
+         in-success
+         in-failure
+         for/first/either
          (contract-out
-          [hash-ref/either ({hash? any/c} {string?} . ->* . (either/c any/c))]
-          [first/either ({list?} {string?} . ->* . (either/c any/c))]
-          [fail-if (boolean? string? . -> . (either/c false?))]))
+          [fail-if (boolean? string? . -> . (either/c false?))]
 
-(require (for-syntax syntax/parse
-                     racket/base)
-         racket/bool
+          [hash-ref/either ({hash? any/c} {string?} . ->* . (either/c any/c))]
+          [first/either ({list?} {string?} . ->* . (either/c any/c))]))
+
+(require racket/bool
          racket/format
          racket/list
-         racket/match)
+         racket/match
+         syntax/parse/define
+         (for-syntax racket/base))
 
 (struct failure (msg) #:prefab)
 
@@ -39,29 +41,125 @@
 
 (define-syntax (either-let* stx)
   (syntax-parse stx
-    [(_ {~optional {~seq #:extra-failure-message extra-msg}}
-        [pat:expr maybe-value:expr] more-clauses ... result)
-     #'(match maybe-value
+    [(_ ([pat:expr maybe-value:expr
+                   {~optional {~seq #:extra-failure-message extra-msg}}]
+         more-clauses ...)
+        body ...)
+     (syntax/loc stx
+       (match maybe-value
          [(failure msg)
           (failure {~? (~a extra-msg msg) msg})]
          [pat
-          (either-let* more-clauses ... result)])]
-    [(_ {~optional {~seq #:extra-failure-message _}} result)
-     #'(success result)]))
-
-(define (hash-ref/either h k [fail-msg @~a{Key '@k' not found}])
-  (match h
-    [(hash-table [(== k) v] _ ...) v]
-    [else                          (failure fail-msg)]))
-
-(define (first/either l [failure-msg "empty"])
-  (if (empty? l) (failure failure-msg) (first l)))
-
-(define (fail-if bool msg)
-  (or bool (failure msg)))
+          (either-let* (more-clauses ...) body ...)]))]
+    [(_ () body ...)
+     (syntax/loc stx
+       (let () body ...))]))
 
 (define (in-success v)
   (in-list (match v
              [(? failure?) empty]
              [else (list v)])))
 
+(define (in-failure v)
+  (in-list (match v
+             [(failure msg) (list msg)]
+             [else empty])))
+
+(define-simple-macro (for/first/either clauses
+                       {~optional {~seq #:failure-message msg}}
+                       body ...)
+  (let/ec return
+    (for* clauses
+      (return (let () body ...)))
+    (failure {~? msg "for/first/either failed"})))
+
+(define (fail-if bool msg)
+  (and bool (failure msg)))
+
+(define (hash-ref/either h k [fail-msg @~a{hash-ref/either: key @~e[k] not found}])
+  (match h
+    [(hash-table [(== k) v] _ ...) v]
+    [else                          (failure fail-msg)]))
+
+(define (first/either l [failure-msg "first/either: empty list"])
+  (if (empty? l) (failure failure-msg) (first l)))
+
+(module+ test
+  (require ruinit)
+  (test-begin
+    #:name either-let*
+    (test-equal? (either-let* () 5) 5)
+    (test-equal? (either-let* ([x 5])
+                              x)
+                 5)
+    (test-equal? (either-let* ([x (failure "bad")])
+                              x)
+                 (failure "bad"))
+    (test-equal? (either-let* ([x 5]
+                               [y (failure "bad")])
+                              (+ x y))
+                 (failure "bad"))
+    (test-equal? (either-let* ([x 5]
+                               [y (+ x 5)])
+                              (+ x y))
+                 15))
+
+  (test-begin
+    #:name fail-if
+    (test-equal? (either-let* ([x 5]
+                               [y (fail-if (odd? x) "bad x")])
+                              (/ x 2))
+                 (failure "bad x"))
+    (test-equal? (either-let* ([x 10]
+                               [y (fail-if (odd? x) "bad x")])
+                              (/ x 2))
+                 5))
+
+  (test-begin
+    #:name in-success
+    (test-equal? (for/list ([x (in-success 5)])
+                   x)
+                 '(5))
+    (test-equal? (for/list ([x (in-success (failure "bad"))])
+                   x)
+                 '()))
+
+  (test-begin
+    #:name in-failure
+    (test-equal? (for/list ([x (in-failure 5)])
+                   x)
+                 '())
+    (test-equal? (for/list ([m (in-failure (failure "bad"))])
+                   m)
+                 '("bad")))
+
+  (test-begin
+    #:name for/first/either
+    (test-equal? (for/first/either ([i '(3 5 7)]
+                                    #:when (even? i))
+                   #:failure-message "no evens!"
+                   i)
+                 (failure "no evens!"))
+    (test-equal? (for/first/either ([i '(3 5 2 7)]
+                                    #:when (even? i))
+                   #:failure-message "no evens!"
+                   i)
+                 2))
+
+  (test-begin
+    #:name hash-ref/either
+    (test-equal? (hash-ref/either (hash) 'a)
+                 (failure "hash-ref/either: key 'a not found"))
+    (test-equal? (hash-ref/either (hash) 'a "bad")
+                 (failure "bad"))
+    (test-equal? (hash-ref/either (hash 'a 5) 'a)
+                 5))
+  
+  (test-begin
+    #:name first/either
+    (test-equal? (first/either empty)
+                 (failure "first/either: empty list"))
+    (test-equal? (first/either empty "bad")
+                 (failure "bad"))
+    (test-equal? (first/either '(5))
+                 5)))
--- option.rkt
+++ option.rkt
@@ -1,13 +1,20 @@
 #lang racket/base
 
+(require racket/contract/base)
+
 (provide absent
          absent?
          option/c
+         option-let*
          in-option
-         for*/option
-         option-let*)
+         for/first/option
+         (contract-out
+          [fail-if (boolean? . -> . (option/c false?))]
 
-(require racket/contract/base
+          [hash-ref/option (hash? any/c . -> . (option/c any/c))]
+          [first/option (list? . -> . (option/c any/c))]))
+
+(require racket/bool
          racket/list
          racket/match
          syntax/parse/define
@@ -17,6 +24,7 @@
   (let ()
     (struct absent ())
     (values (absent) absent?)))
+
 (define (option/c inner/c)
   (or/c absent? inner/c))
 
@@ -24,16 +32,97 @@
   (in-list (match o
              [(? absent?) empty]
              [else (list o)])))
-(define-simple-macro (for*/option clauses body ...)
+
+(define-simple-macro (for/first/option clauses body ...)
   (let/ec return
     (for* clauses
       (return (let () body ...)))
     absent))
-(define-simple-macro (option-let* ([name maybe-option] more ...) body ...)
-  #:with result-expr (if (null? (attribute more))
-                         #'(let () body ...)
-                         #'(option-let* (more ...) body ...))
-  (let ([name maybe-option])
-    (if (absent? name)
-        absent
-        result-expr)))
+
+(define-syntax (option-let* stx)
+  (syntax-parse stx
+    [(_ ([pat:expr maybe-value:expr]
+         more-clauses ...)
+        body ...)
+     (syntax/loc stx
+       (match maybe-value
+         [(? absent? a) a]
+         [pat (option-let* (more-clauses ...) body ...)]))]
+    [(_ () body ...)
+     (syntax/loc stx
+       (let () body ...))]))
+
+(define (fail-if bool)
+  (and bool absent))
+
+(define (hash-ref/option h k)
+  (match h
+    [(hash-table [(== k) v] _ ...) v]
+    [else                          absent]))
+
+(define (first/option l)
+  (if (empty? l) absent (first l)))
+
+(module+ test
+  (require ruinit)
+
+  (test-begin
+    #:name in-option
+    (test-equal? (for/list ([x (in-option 5)]) x)
+                 '(5))
+    (test-equal? (for/list ([x (in-option absent)]) x)
+                 '()))
+
+  (test-begin
+    #:name for/first/option
+    (test-equal? (for/first/option ([i '(3 5 7)]
+                                    #:when (even? i))
+                   i)
+                 absent)
+    (test-equal? (for/first/option ([i '(3 5 2 7)]
+                                    #:when (even? i))
+                   i)
+                 2))
+
+  (test-begin
+    #:name option-let*
+    (test-equal? (option-let* () 5) 5)
+    (test-equal? (option-let* ([x 5])
+                              x)
+                 5)
+    (test-equal? (option-let* ([x absent])
+                              x)
+                 absent)
+    (test-equal? (option-let* ([x 5]
+                               [y absent])
+                              (+ x y))
+                 absent)
+    (test-equal? (option-let* ([x 5]
+                               [y (+ x 5)])
+                              (+ x y))
+                 15))
+
+  (test-begin
+    #:name fail-if
+    (test-equal? (option-let* ([x 5]
+                               [y (fail-if (odd? x))])
+                              (/ x 2))
+                 absent)
+    (test-equal? (option-let* ([x 10]
+                               [y (fail-if (odd? x))])
+                              (/ x 2))
+                 5))
+
+  (test-begin
+    #:name hash-ref/option
+    (test-equal? (hash-ref/option (hash) 'a)
+                 absent)
+    (test-equal? (hash-ref/option (hash 'a 5) 'a)
+                 5))
+  
+  (test-begin
+    #:name first/option
+    (test-equal? (first/option empty)
+                 absent)
+    (test-equal? (first/option '(5))
+                 5)))
